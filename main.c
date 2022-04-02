5ebf682befa00db5347a297d93d6348e77b6868b
Artem Shoobovych <shybovycha@gmail.com>
2022-04-02 18:54:03 +1100

    Working code

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "esp_bt_main.h"
 #include "esp_bt_device.h"
+#include "esp_bt.h"
 #include "esp_gap_bt_api.h"
+#include "esp_gap_ble_api.h"
 #include "esp_a2dp_api.h"
 
 #include <EEPROM.h>
@@ -58,7 +60,88 @@ void bt_data_cb(const uint8_t *data, uint32_t len) {
   }
 }*/
 
+void bt_evt_cb(esp_gap_ble_cb_event_t eventType, esp_ble_gap_cb_param_t *p_param) {
+  Serial.printf("Caught BLE event %d\n", (int) eventType);
+  
+  // switch (eventType) {
+  //  case BT_APP_EVT_STACK_UP:
+      
+  // }
+}
+
 void setup() {
+  Serial.begin(115200);
+
+  /*esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+
+  // wait for BT controller to become available and idle
+  if (esp_bt_controller_get_status() != ESP_BT_CONTROLLER_STATUS_ENABLED) {
+    if (esp_bt_controller_get_status() == ESP_BT_CONTROLLER_STATUS_IDLE){
+        if (esp_bt_controller_init(&bt_cfg) != ESP_OK) {
+          Serial.printf("Initializing BT controller failed (1)\n");
+        }
+        
+        while (esp_bt_controller_get_status() == ESP_BT_CONTROLLER_STATUS_IDLE);
+    }
+    
+    if (esp_bt_controller_get_status() == ESP_BT_CONTROLLER_STATUS_INITED) {
+        if (esp_bt_controller_enable(ESP_BT_MODE_BTDM)) {
+            Serial.printf("Enabling BT controller failed (2)\n");
+            return;
+        }
+    }
+    
+    if (esp_bt_controller_get_status() != ESP_BT_CONTROLLER_STATUS_ENABLED){
+      Serial.printf("Getting a grip of BT controller failed (3)\n");
+      return;
+    }
+  }*/
+
+  btStart();
+
+  if (esp_bluedroid_init() != ESP_OK) {
+    Serial.printf("Initializing Bluedroid failed\n");
+    return;
+  }
+
+  if (esp_bluedroid_enable() != ESP_OK) {
+    Serial.printf("Enabling Bluedroid failed\n");
+    return;
+  }
+
+  Serial.printf("BT stack is up\n");
+
+  // setup BT device descriptor
+  esp_bt_cod_t cod;
+  
+  esp_bt_gap_get_cod(&cod);
+  cod.minor |= 6; // headphones
+  cod.major |= ESP_BT_COD_MAJOR_DEV_AV ;
+  cod.service |= ESP_BT_COD_SRVC_RENDERING | ESP_BT_COD_SRVC_AUDIO;
+  
+  esp_bt_gap_set_cod(cod, ESP_BT_INIT_COD);
+  
+  // set device name
+  esp_bt_dev_set_device_name("ESP32 SPEAKER");
+  
+  // initialize i2s / adp
+  // esp_a2d_register_callback(&bt_app_a2d_cb);
+  // initialize A2DP sink and set the data callback(A2DP is bluetooth audio)
+  esp_a2d_sink_register_data_callback(bt_data_cb);
+
+  esp_a2d_sink_init();
+
+  // set scan mode
+  // set discoverable and connectable mode, wait to be connected
+  esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
+
+  // register BT event callback
+  esp_ble_gap_register_callback(&bt_evt_cb);
+
+  // ==================================================================================================
+  // set up bluetooth classic via bluedroid
+  // btStart();
+
   static const uint16_t BITRATE = 44100;
 
   // i2s configuration
@@ -67,11 +150,11 @@ void setup() {
     .sample_rate = BITRATE,
     .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
     .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-    .communication_format = static_cast<i2s_comm_format_t>(I2S_COMM_FORMAT_STAND_I2S | I2S_COMM_FORMAT_I2S_MSB),
+    .communication_format = static_cast<i2s_comm_format_t>(I2S_COMM_FORMAT_I2S_MSB),
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // default interrupt priority
-    .dma_buf_count = 64,
-    .dma_buf_len = 256, // == dma_frame_num = channel_format (alias for num_channels) * bits_per_sample = bits
-    .use_apll = true,
+    .dma_buf_count = 32,
+    .dma_buf_len = 512, // == dma_frame_num = channel_format (alias for num_channels) * bits_per_sample = bits
+    .use_apll = false,
     .tx_desc_auto_clear = false
     // .fixed_mclk = -1
   };
@@ -86,6 +169,8 @@ void setup() {
 
   esp_err_t err;
 
+  Serial.printf("Installing I2S driver\n");
+
   // now configure i2s with constructed pinout and config
   err = i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
 
@@ -99,68 +184,10 @@ void setup() {
     Serial.printf("Failed setting i2s pin: %d\n", err);
   }
 
-  // i2s_set_clk(I2S_NUM_0, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_STEREO);
-  // i2s_set_sample_rates(I2S_NUM_0, 44100);
-
-  err = i2s_set_clk(I2S_NUM_0, BITRATE, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_STEREO);
-
-  if (err != ESP_OK) {
-    Serial.printf("Failed setting i2s clk: %d\n", err);
-  }
-
-  err = i2s_set_sample_rates(I2S_NUM_0, BITRATE);
-
-  if (err != ESP_OK) {
-    Serial.printf("Failed setting i2s sample rate: %d\n", err);
-  }
-
-  // set up bluetooth classic via bluedroid
-  btStart();
-
-  esp_bluedroid_init();
-  esp_bluedroid_enable();
-
-  esp_bt_cod_t cod;
-  esp_bt_gap_get_cod(&cod);
-  cod.minor |= 6; // headphones
-  cod.major |= ESP_BT_COD_MAJOR_DEV_AV ;
-  cod.service |= ESP_BT_COD_SRVC_RENDERING | ESP_BT_COD_SRVC_AUDIO;
-  esp_bt_gap_set_cod(cod, ESP_BT_INIT_COD);
-
-  // set up device name
-  const char *dev_name = "ESP_SPEAKER";
-  esp_bt_dev_set_device_name(dev_name);
-
-  // initialize A2DP sink and set the data callback(A2DP is bluetooth audio)
-  esp_a2d_sink_register_data_callback(bt_data_cb);
-  /*esp_a2d_register_callback(bt_event_cb);
-
-  esp_a2d_sink_init();
-
-  esp_bd_addr_t lastConnectionAddr;
-
-  uint8_t wasConnected = EEPROM.read(0);
-
-  if (wasConnected) {
-    for (int i = 0; i < ESP_BD_ADDR_LEN; ++i)
-    {
-      lastConnectionAddr[i] = EEPROM.read(EEPROM_LAST_CONN_ADDR_OFFSET + i);
-    }
-
-    err = esp_a2d_sink_connect(lastConnectionAddr);
-
-    if (err != ESP_OK) {
-      Serial.printf("Failed connecting to last connection: %d\n", err);
-    }
-  }*/
-
-  // set discoverable and connectable mode, wait to be connected
-  esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
-
-  Serial.begin(115200);
+  Serial.printf("Starting bt\n");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(1000);
+  delay(100);
 }
